8c02b08cfca4326e46c4ac74a03f19db3e3c2fe3
HungKuen <magnus.kull@sanitarium.se>
2018-11-14 12:57:46 +0100

    added to display last modified date of the page automatic

--- js/education.js
+++ js/education.js
@@ -1,5 +1,51 @@
 $(document).ready(function () {
 /*
+ *Set id date to last modified month and year
+ */
+    let myDate = document.lastModified;
+    let month
+    switch(parseInt(myDate.slice(0,2))) {
+        case 1:
+            month = "Januari";
+            break;
+        case 2:
+            month = "Februari";
+            break;
+        case 3:
+            month = "Mars";
+            break;
+        case 4:
+            month = "April";
+            break;
+        case 5:
+            month = "Maj";
+            break;
+        case 6:
+            month = "Juni";
+            break;
+        case 7:
+            month = "Juli";
+            break;
+        case 8:
+            month = "Augusti";
+            break;
+        case 9:
+            month = "September";
+            break;
+        case 10:
+            month = "Oktober";
+            break;
+        case 11:
+            month = "November";
+            break;
+        case 12:
+            month = "December";
+            break;
+
+    }
+    document.getElementById("date").innerHTML = ("Uppdaterades: " + month + " " + myDate.slice(6,10));
+
+/*
  *Makes so it can only show one education at a time
  */
     $("#toggleTechnicalSkills").click(function () {
@@ -41,3 +87,8 @@ $(document).ready(function () {
         $(".otherMerits").toggle();
     });
 });
+
+
+
+
+
